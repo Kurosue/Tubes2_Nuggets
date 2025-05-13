72fb6d76ff74f71cce5a52f838e404c05a47007f
Kurosue <adityarahmadeni30@gmail.com>
2025-05-13 16:15:19 +0700

    chore: cleaning code

--- src/backend/utils/bfs_multi.go
+++ src/backend/utils/bfs_multi.go
@@ -5,40 +5,30 @@ import (
 	"sync"
 )
 
-// BFSNRecipesMultithreaded is a multithreaded version of BFSNRecipes
-// It finds multiple recipe paths to create the target element
 func BFSNRecipes(target string, Elements map[string]Element, maxRecipes int, visitedNode *int) []*Node {
 	var resultRoots []*Node
 	var resultMutex sync.Mutex
 	queue := []*Node{{Result: target, Depth: 0}}
 	var queueMutex sync.Mutex
 	
-	// Use a wait group to track all goroutines
 	var wg sync.WaitGroup
 	
-	// Create a semaphore to limit the number of concurrent goroutines
-	// This prevents creating too many goroutines which could overwhelm the system
-	maxWorkers := 10 // Adjust this based on your system capabilities
+	maxWorkers := 10
 	semaphore := make(chan struct{}, maxWorkers)
 	
-	// Flag to signal that we've reached max recipes
 	done := make(chan struct{})
 	isDone := false
 	
-	// Process function that will be run concurrently
 	processNode := func(current *Node) {
 		defer wg.Done()
-		defer func() { <-semaphore }() // Release the semaphore when done
+		defer func() { <-semaphore }()
 		
-		// Check if we're done before processing
 		select {
 		case <-done:
 			return
 		default:
-			// Continue processing
 		}
 		
-		// Track visited nodes
 		resultMutex.Lock()
 		*visitedNode++
 		resultMutex.Unlock()
@@ -78,11 +68,9 @@ func BFSNRecipes(target string, Elements map[string]Element, maxRecipes int, vis
 				continue
 			}
 			
-			// Process ingredient search in parallel
 			var leftMutex, rightMutex sync.Mutex
 			var left, right *Node
 			
-			// Use wait group for ingredient searches
 			var ingWg sync.WaitGroup
 			ingWg.Add(2)
 			
@@ -104,7 +92,6 @@ func BFSNRecipes(target string, Elements map[string]Element, maxRecipes int, vis
 				rightMutex.Unlock()
 			}()
 			
-			// Wait for both ingredient searches to complete
 			ingWg.Wait()
 			
 			newNode := &Node{
@@ -128,17 +115,13 @@ func BFSNRecipes(target string, Elements map[string]Element, maxRecipes int, vis
 		}
 	}
 	
-	// Main processing loop
 	for len(queue) > 0 {
-		// Check if we're done
 		select {
 		case <-done:
 			break
 		default:
-			// Continue processing
 		}
 		
-		// Get next node from queue
 		queueMutex.Lock()
 		if len(queue) == 0 {
 			queueMutex.Unlock()
@@ -148,23 +131,18 @@ func BFSNRecipes(target string, Elements map[string]Element, maxRecipes int, vis
 		queue = queue[1:]
 		queueMutex.Unlock()
 		
-		// Add this task to our wait group
 		wg.Add(1)
 		
-		// Acquire semaphore (blocks if maxWorkers goroutines are already running)
 		semaphore <- struct{}{}
 		
-		// Process this node in a goroutine
 		go processNode(current)
 	}
 	
-	// Wait for all goroutines to complete
 	wg.Wait()
 	
 	return resultRoots
 }
 
-// FlattenMultipleTrees flattens multiple recipe trees in parallel
 func FlattenMultipleTrees(roots []*Node) [][]Message {
 	result := make([][]Message, len(roots))
 	var wg sync.WaitGroup
